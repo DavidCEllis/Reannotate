f54ec45c4984af579ef0e5a6c3bd43260fcec727
David C Ellis <ducksual@gmail.com>
2026-03-13 20:47:29 +0000

    Include all arguments for signature

--- src/reannotate/inspect_signature_patch.py
+++ src/reannotate/inspect_signature_patch.py
@@ -1,18 +1,30 @@
 # Patched version of inspect.signature that uses deferred annotations
+# This works by making copies of the relevant functions and patching their
+# usage of get_annotations to get_deferred_annotations with additional
+# arguments ignored.
+__lazy_modules__ = ["collections.abc", "typing"]
+
+import annotationlib
 import types
 import typing as t
 import inspect
 
-from . import get_deferred_annotations
+from collections.abc import Callable, Mapping
+
+from . import get_deferred_annotations, DeferredAnnotation
 
 
-def _false_get_annotations(obj, **kwargs):
+type _IntrospectableCallable = Callable[..., t.Any]
+
+
+def _false_get_annotations(obj: t.Any, **kwargs) -> dict[str, DeferredAnnotation]:
     # A wrapper for get_deferred_annotations that takes kwargs but uses none
     return get_deferred_annotations(obj)
 
+
 def get_patched_function(
     func: types.FunctionType,
-    patch_dict: dict[str, t.Any]
+    patch_dict: dict[str, t.Any],
 ) -> types.FunctionType:
     patched_globs = func.__globals__ | patch_dict
     new_func = types.FunctionType(
@@ -27,16 +39,39 @@ def get_patched_function(
     patch_dict[func.__name__] = new_func
     return new_func
 
+
 patch_dict = {
     "get_annotations": _false_get_annotations,
 }
 
-# Patched versions of all of the relevant inspect classes
-deferred_signature_from_function = get_patched_function(inspect._signature_from_function, patch_dict)
-deferred_signature_from_callable = get_patched_function(inspect._signature_from_callable, patch_dict)
+# Patch the relevant inspect functions.
+# _signature_from_function is used by _signature_from_callable
+_deferred_signature_from_function = get_patched_function(
+    inspect._signature_from_function,
+    patch_dict,
+)
+_deferred_signature_from_callable = get_patched_function(
+    inspect._signature_from_callable,
+    patch_dict,
+)
 
-def signature(obj, *, follow_wrapped=True):
-    return deferred_signature_from_callable(
+
+def signature(
+    obj: _IntrospectableCallable,
+    *,
+    follow_wrapped: bool = True,
+    globals: Mapping[str, t.Any] | None = None,
+    locals: Mapping[str, t.Any] | None = None,
+    eval_str: bool = False,
+    annotation_format: annotationlib.Format = annotationlib.Format.VALUE,
+) -> inspect.Signature:
+    """
+    Return a signature with deferred annotations
+
+    This has the same arguments as inspect.signature but only `obj`
+    and `follow_wrapped` are used.
+    """
+    return _deferred_signature_from_callable(
         obj,
         sigcls=inspect.Signature,
         follow_wrapper_chains=follow_wrapped,
