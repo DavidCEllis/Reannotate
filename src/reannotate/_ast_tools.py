0251510fcd9c83adb69678d016f65600df7d25f2
David C Ellis <ducksual@gmail.com>
2026-04-20 22:55:46 +0100

    remove unnecessary and incorrect position fixup

--- src/reannotate/_ast_tools.py
+++ src/reannotate/_ast_tools.py
@@ -13,7 +13,5 @@ class NameReplacer(ast.NodeTransformer):
 
     def visit_Name(self, node: ast.Name):
         if (new_name := self._names.get(node.id, _sentinel)) is not _sentinel:
-            new_node = ast.Name(id=type_repr(new_name))
-            ast.copy_location(node, new_node)
-            node = new_node
+            node = ast.Name(id=type_repr(new_name))
         return node
